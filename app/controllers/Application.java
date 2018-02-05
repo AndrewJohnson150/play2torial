8cc0e6e6597c392a0dafd31202a409842ea0ce8a
AndrewJohnson150 <andrewjohnson150@gmail.com>
2018-02-05 13:03:55 -0700

    added logging

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,5 +1,10 @@
 package controllers;
 
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import play.*;
 import play.mvc.*;
 
@@ -7,11 +12,14 @@ import views.html.*;
 
 public class Application extends Controller {
 
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
     public static Result index() {
         return ok(index.render("hello, world", play.data.Form.form(models.Task.class)));
     }
 
     public static Result addTask() {
+        log.info("Adding a task");
         play.data.Form<models.Task> form = play.data.Form.form(models.Task.class).bindFromRequest();
         if (form.hasErrors()) {
             return badRequest(index.render("hello, world", form));
@@ -21,6 +29,11 @@ public class Application extends Controller {
             task.save();
             return redirect(routes.Application.index());
         }
+    }
+
+    public static void clearTasks() {
+        java.util.List<models.Task> tasks = new play.db.ebean.Model.Finder(String.class, models.Task.class).all();
+        tasks = null;
     }
 
     public static Result getTasks() {
